bd57b017263c91ea2fa2f44fb106b2c183378f03
Mikki Jones <mvjones150@outlook.com>
2021-04-05 22:34:32 -0700

    added support for scss

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'css'),
+      path.join(__dirname, 'components/*')
+    ],
+  },
+}
